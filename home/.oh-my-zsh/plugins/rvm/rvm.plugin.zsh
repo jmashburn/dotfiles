a61749c157289c69662a99bd344b100e6b8da5a8
Moncef Belyamani <giter@done>
2013-05-28 09:25:50 -0400

    upgrade oh-my-zsh

--- home/.oh-my-zsh/plugins/rvm/rvm.plugin.zsh
+++ home/.oh-my-zsh/plugins/rvm/rvm.plugin.zsh
@@ -3,9 +3,9 @@ fpath=($rvm_path/scripts/zsh/Completion $fpath)
 alias rubies='rvm list rubies'
 alias gemsets='rvm gemset list'
 
-local ruby18='ruby-1.8.7-p334'
-local ruby19='ruby-1.9.3-p385'
-local ruby20='ruby-2.0.0-rc2'
+local ruby18='ruby-1.8.7-p371'
+local ruby19='ruby-1.9.3-p392'
+local ruby20='ruby-2.0.0-p0'
 
 function rb18 {
 	if [ -z "$1" ]; then
@@ -61,3 +61,5 @@ function _rvm_completion {
   source $rvm_path"/scripts/zsh/Completion/_rvm"
 }
 compdef _rvm_completion rvm
+
+function rvmg	 { rvm --create --ruby-version use $1 }
