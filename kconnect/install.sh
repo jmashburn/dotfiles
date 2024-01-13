9f604447c6e890fc6b56469d1115855c0658a886
Jared Mashburn <jmashburn@iwobble.com>
2024-01-13 12:44:42 -0500

    Adding kconnect to my dotfiles

---
+++ kconnect/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# KConnect
+#
+# This installs some of the command dependencies for kconnect
+#
+#
+
+# Check for kconnect
+BIN_DIR="$HOME/.bin/"
+
+if [[ ! -d $BIN_DIR ]]
+then
+    mkdir $BIN_DIR
+fi
+
+if test ! $(which kcaonnect)
+then
+    echo "   Installing kConnect"
+    cd $BIN_DIR
+
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/fidelity/kconnect/main/scripts/install-kconnect.sh)"
+
+fi
+exit 0
