cb468e40571f83030a7a608c984653395de9e5b9
Jared Mashburn <jmashburn@iwobble.com>
2025-06-16 20:14:38 -0400

    Updating _pathmunge -q to not echo the path

--- lib/helpers.bash
+++ lib/helpers.bash
@@ -237,12 +237,14 @@ function _egrep() {
 }
 
 function _pathmunge() {
-    if ! echo $PATH | egrep "(^|:)$1($|:)" ; then
+    if [ -e $1 ]; then
+    if ! echo $PATH | egrep -q "(^|:)$1($|:)" ; then
         if [ "$2" = "after" ] ; then
             PATH=$PATH:$1
         else
             PATH=$1:$PATH
         fi
+    fi
     fi
 }
 
